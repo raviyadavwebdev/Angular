751f2870f96961f2b09c6880353f6657a21a25be
Ravi Yadav <raviyadavwebdev@gmail.com>
2018-02-18 11:02:42 +0530

    Recipe list css

--- src/app/components/recipe/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe/recipe-list/recipe-list.component.ts
@@ -7,9 +7,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Chilli Dogs', 'Not literally a dog, please chillax', `https://leitesculinaria.com/wp-content/
-    uploads/fly-images/103748/chili-dogs-recipe-400x300-c.jpg`),
-    new Recipe('Pan Cakes', 'Delicious sweet Pan Cakes umm yeah',
+    new Recipe('Chilli Dogs', 'Not literally a dog, please chillax. The name was taken randomly. I absolutely love dogs bro', `https://leitesculinaria.com/wp-content/uploads/fly-images/103748/chili-dogs-recipe-400x300-c.jpg`),
+    new Recipe('Pan Cakes', 'Delicious sweet Pan Cakes umm yeah. Let me know in the mail what addons you would like',
      'http://www.sigmalive.com/en/application/cache/default/images/news/400x300/cake.png')
   ];
   constructor() { }
